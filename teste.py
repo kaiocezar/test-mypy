e6b526c6e4f22b8e18f5534d7dfe7228b545fad5
Kaio Cezar <kaiocezar.7@gmail.com>
2025-04-09 21:25:33 -0300

    erro mypy

--- teste.py
+++ teste.py
@@ -2,5 +2,5 @@ def greeting(name: str) -> str:
     return 'Hello ' + name
 
 
-def bad_greeting(name: int) -> str:
+def bad_greeting(name: str) -> str:
     return 'Hello ' * name  # Unsupported operand types for * ("str" and "str")
